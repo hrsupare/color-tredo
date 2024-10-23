07aec3a27359b599c80304ee0e3078be519485f1
pwanjari23 <pratikshawanjari23@gmail.com>
2024-10-23 11:53:48 +0530

    changes in navbardefault

--- src/Components/Navbar/NavbarDefault.jsx
+++ src/Components/Navbar/NavbarDefault.jsx
@@ -198,7 +198,7 @@ import React from 'react';
 
 export function NavbarDefault() {
   return (
-    <div className="flex justify-center mt-2 sm:mt-4">
+    <div className="flex justify-center sm:mt-2">
       <div className="font-bold text-xl sm:text-lg w-full max-w-xs sm:max-w-sm text-center bg-gradient-to-r from-indigo-500 to-indigo-100 hover:from-indigo-600 hover:to-indigo-200 p-3 sm:p-4 rounded-xl">
         Parity
       </div>
